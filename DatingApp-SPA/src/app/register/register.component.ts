6edacc8241dd4368b93cfe734923c12f86057307
Christian Maddox <christian.maddox93@gmail.com>
2019-05-28 13:10:34 -0400

    Added reactive forms to the register page. Added some fields, made them reactive, and added ngx-bootstrap datepicker for DateOfBirth.

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+    FormGroup,
+    FormControl,
+    Validators,
+    FormBuilder
+} from '@angular/forms';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker/public_api';
 
 @Component({
     selector: 'app-register',
@@ -10,23 +17,61 @@ import { AlertifyService } from '../_services/alertify.service';
 export class RegisterComponent implements OnInit {
     @Output() cancelRegister = new EventEmitter();
     model: any = {};
+    registerForm: FormGroup;
+    bsConfig: Partial<BsDatepickerConfig>;
 
     constructor(
         private authService: AuthService,
-        private alertify: AlertifyService
+        private alertify: AlertifyService,
+        private fb: FormBuilder
     ) {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.bsConfig = {
+            containerClass: 'theme-default'
+        }
+        this.createRegisterForm();
+    }
+
+    createRegisterForm() {
+        this.registerForm = this.fb.group(
+            {
+                gender: ['male'],
+                username: ['', Validators.required],
+                knownAs: ['', Validators.required],
+                dateOfBirth: [null, Validators.required],
+                city: ['', Validators.required],
+                country: ['', Validators.required],
+                password: [
+                    '',
+                    [
+                        Validators.required,
+                        Validators.minLength(4),
+                        Validators.maxLength(8)
+                    ]
+                ],
+                confirmPassword: ['', Validators.required]
+            },
+            { validator: this.passwordMatchValidator }
+        );
+    }
+
+    passwordMatchValidator(g: FormGroup) {
+        return g.get('password').value === g.get('confirmPassword').value
+            ? null
+            : { mismatch: true };
+    }
 
     register() {
-        this.authService.register(this.model).subscribe(
-            () => {
-                this.alertify.success('Registration successful');
-            },
-            error => {
-                this.alertify.error(error);
-            }
-        );
+        // this.authService.register(this.model).subscribe(
+        //     () => {
+        //         this.alertify.success('Registration successful');
+        //     },
+        //     error => {
+        //         this.alertify.error(error);
+        //     }
+        // );
+        console.log(this.registerForm.value);
     }
 
     cancel() {
--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -9,6 +9,6 @@ namespace DatingApp.API.Dtos
 
         [Required]
         [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify a password between 4 and 8 characters.")]
-        public string Password { get; set; }
+        public string Password { get; set; }        
     }
 }
