423a43ea632284e58953a5700abee1b760ab34b0
Christian Maddox <christian.maddox93@gmail.com>
2019-05-28 13:57:13 -0400

    Hooked up updated register component to the API. Changed register in the RegisterComponent to use the registerForm when building the register request.

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -5,9 +5,11 @@ import {
     Validators,
     FormBuilder
 } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker/public_api';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
-import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker/public_api';
+import { User } from '../_models/user';
 
 @Component({
     selector: 'app-register',
@@ -16,20 +18,21 @@ import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker/public_api';
 })
 export class RegisterComponent implements OnInit {
     @Output() cancelRegister = new EventEmitter();
-    model: any = {};
+    user: User;
     registerForm: FormGroup;
     bsConfig: Partial<BsDatepickerConfig>;
 
     constructor(
         private authService: AuthService,
         private alertify: AlertifyService,
-        private fb: FormBuilder
+        private fb: FormBuilder,
+        private router: Router
     ) {}
 
     ngOnInit() {
         this.bsConfig = {
             containerClass: 'theme-default'
-        }
+        };
         this.createRegisterForm();
     }
 
@@ -63,15 +66,22 @@ export class RegisterComponent implements OnInit {
     }
 
     register() {
-        // this.authService.register(this.model).subscribe(
-        //     () => {
-        //         this.alertify.success('Registration successful');
-        //     },
-        //     error => {
-        //         this.alertify.error(error);
-        //     }
-        // );
-        console.log(this.registerForm.value);
+        if (this.registerForm.valid) {
+            this.user = Object.assign({}, this.registerForm.value);
+            this.authService.register(this.user).subscribe(
+                () => {
+                    this.alertify.success('Registration successful');
+                },
+                error => {
+                    this.alertify.error(error);
+                },
+                () => {
+                    this.authService.login(this.user).subscribe(() => {
+                        this.router.navigate(['/members']);
+                    });
+                }
+            );
+        }
     }
 
     cancel() {
